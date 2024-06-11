1a9e2da6e2400932367e5ed364b349eb15f14e49
sosuu <c0b23062ae@edu.teu.ac.jp>
2024-06-11 16:32:22 +0900

    練習７：背景画像がループしなくなったよ

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     koukaton3_img = pg.image.load("fig/3.png")
     koukaton3_img = pg.transform.flip(koukaton3_img, True, False)
     tmr = 0
@@ -17,8 +18,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr % 800
+        x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_img2, [-x+4800, 0])
         koukaton3_rect = koukaton3_img.get_rect()   # こうかとんRectの抽出
         koukaton3_rect.center = 300, 200            # こうかとんの中心座標を300, 200に設定
         screen.blit(koukaton3_img, koukaton3_rect)  # こうかとんをRectに従って貼り付ける
