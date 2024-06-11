8cf096ad0658f00f26d41f0a3ff6ee958c920b16
sosuu <c0b23062ae@edu.teu.ac.jp>
2024-06-11 17:36:06 +0900

    演習２：move_ipメソッドの使用を１回だけに変更したよ

--- pygame_image.py
+++ pygame_image.py
@@ -16,7 +16,9 @@ def main():
     koukaton3_rect = koukaton3_img.get_rect()   # こうかとんRectの抽出
     koukaton3_rect.center = 300, 200            # こうかとんの中心座標を300, 200に設定
     tmr = 0
-    while True:
+    koukaton_x = 0
+    koukaton_y = 0
+    while True or num == 0:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
@@ -28,15 +30,23 @@ def main():
 
         key_lst = pg.key.get_pressed()              # 全キーの押下状態を取得
         if key_lst[pg.K_UP]:
-            koukaton3_rect.move_ip((0, -1))
+            koukaton_x = 0
+            koukaton_y = -1
         elif key_lst[pg.K_DOWN]:
-            koukaton3_rect.move_ip((0, +1))
+            koukaton_x = 0
+            koukaton_y = +1
         elif key_lst[pg.K_LEFT]:
-            koukaton3_rect.move_ip((-1, 0))
+            koukaton_x = -1
+            koukaton_y = 0
         elif key_lst[pg.K_RIGHT]:
-            koukaton3_rect.move_ip((+2, 0))
-        koukaton3_rect.move_ip((-1, 0))
+            koukaton_x = +2
+            koukaton_y = 0
         
+        koukaton3_rect.move_ip((koukaton_x, koukaton_y))
+        koukaton_x = 0
+        koukaton_y = 0
+        koukaton3_rect.move_ip((-1, 0))
+
         screen.blit(koukaton3_img, koukaton3_rect)  # こうかとんをRectに従って貼り付ける
         pg.display.update()
         tmr += 1        
